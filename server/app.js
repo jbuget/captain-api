bfc08232fd029d32206e9c2f9dc4eecaecb09623
Jérémy Buget <jbuget@gmail.com>
2019-07-19 16:12:58 +0200

    Add Sequelize ORM

--- server/app.js
+++ server/app.js
@@ -1,5 +1,70 @@
-const express = require('express')
-const app = express()
+
+/* Sequelize */
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres://postgres:example@localhost/granny');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const Model = Sequelize.Model;
+class User extends Model {}
+User.init({
+  // attributes
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    // allowNull defaults to true
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+}, {
+  sequelize,
+  modelName: 'user'
+  // options
+});
+
+/* Passport */
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+/* Express */
+
+const express = require('express');
+
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
