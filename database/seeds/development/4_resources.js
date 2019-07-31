2034386519616398fffb3822d2f9d90fe5ab75ed
Jérémy Buget <jbuget@gmail.com>
2019-07-31 15:38:00 +0200

    Add seeds for Resources

--- database/seeds/development/4_resources.js
+++ database/seeds/development/4_resources.js
@@ -6,15 +6,7 @@ exports.seed = function(knex) {
       return knex('resources').insert([
         {
           team_id: 1,
-          name: 'Google',
-          method: 'GET',
-          url: 'https://google.com',
-          headers: null,
-          body: null,
-        },
-        {
-          team_id: 1,
-          name: 'Granny',
+          name: 'Get API data',
           method: 'GET',
           url: 'https://granny.netlify.com',
           headers: null,
@@ -22,11 +14,105 @@ exports.seed = function(knex) {
         },
         {
           team_id: 1,
-          name: 'Authenticate',
+          name: 'Create an account',
           method: 'POST',
+          url: 'http://localhost:3000/users',
+          headers: JSON.stringify({
+            "Content-Type": "application/json"
+          }),
+          body: JSON.stringify({
+            "name": "Alice",
+            "email": "alice@example.net",
+            "password": "abcd1234"
+          }),
+        },
+        {
+          team_id: 1,
+          name: 'Validate a created account',
+          method: 'POST',
+          url: 'http://localhost:3000/users/account-validation',
+          headers: JSON.stringify({
+            "Content-Type": "application/json"
+          }),
+          body: JSON.stringify({
+            "userId": 4,
+            "uuid": "630eb68f-e0fa-5ecc-887a-7c7a62614681"
+          }),
+        },
+        {
+          team_id: 1,
+          name: 'Login',
+          method: 'GET',
           url: 'http://localhost:3000/token',
-          headers: null,
-          body: '',
+          headers: JSON.stringify({
+            "Content-Type": "application/x-www-form-urlencoded"
+          }),
+          body: JSON.stringify({
+            "username": "alice@example.net",
+            "password": "abcd1234"
+          }),
+        },
+        {
+          team_id: 1,
+          name: 'Get account information',
+          method: 'GET',
+          url: 'http://localhost:3000/users/me',
+          headers: JSON.stringify({
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJHcmFubnkuanMiLCJzdWIiOjEsIm5hbWUiOiJBbGljZSBKb2huc29uIiwiZW1haWwiOiJhbGljZUBncmFubnkuanMiLCJpYXQiOjE1NjQ0NzU1MDF9.3VGPg-Zgb3XKaXUrcRTJYLjYFOQiB-axwahnvLXQMYs",
+            "Content-Type": "application/json"
+          }),
+          body: JSON.stringify({
+            "userId": 4,
+            "uuid": "630eb68f-e0fa-5ecc-887a-7c7a62614681"
+          }),
+        },
+        {
+          team_id: 1,
+          name: 'Update an account',
+          method: 'PATCH',
+          url: 'http://localhost:3000/users/me',
+          headers: JSON.stringify({
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJHcmFubnkuanMiLCJzdWIiOjEsIm5hbWUiOiJBbGljZSBKb2huc29uIiwiZW1haWwiOiJhbGljZUBncmFubnkuanMiLCJpYXQiOjE1NjQ0NzU1MDF9.3VGPg-Zgb3XKaXUrcRTJYLjYFOQiB-axwahnvLXQMYs",
+            "Content-Type": "application/json"
+          }),
+          body: {
+            "name": "Alice Smith",
+          },
+        },
+        {
+          team_id: 1,
+          name: 'Delete an account',
+          method: 'DELETE',
+          url: 'http://localhost:3000/users/me',
+          headers: JSON.stringify({
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJHcmFubnkuanMiLCJzdWIiOjEsIm5hbWUiOiJBbGljZSBKb2huc29uIiwiZW1haWwiOiJhbGljZUBncmFubnkuanMiLCJpYXQiOjE1NjQ0NzU1MDF9.3VGPg-Zgb3XKaXUrcRTJYLjYFOQiB-axwahnvLXQMYs",
+            "Content-Type": "application/json"
+          }),
+          body: null,
+        },
+        {
+          team_id: 1,
+          name: 'Create a team',
+          method: 'POST',
+          url: 'http://localhost:3000/teams',
+          headers: JSON.stringify({
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJHcmFubnkuanMiLCJzdWIiOjEsIm5hbWUiOiJBbGljZSBKb2huc29uIiwiZW1haWwiOiJhbGljZUBncmFubnkuanMiLCJpYXQiOjE1NjQ0NzU1MDF9.3VGPg-Zgb3XKaXUrcRTJYLjYFOQiB-axwahnvLXQMYs",
+            "Content-Type": "application/json"
+          }),
+          body: JSON.stringify({
+            "name": "Delta Corp"
+          }),
+        },
+        {
+          team_id: 1,
+          name: 'Get user’s teams',
+          method: 'GET',
+          url: 'http://localhost:3000/me/teams',
+          headers: JSON.stringify({
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJHcmFubnkuanMiLCJzdWIiOjEsIm5hbWUiOiJBbGljZSBKb2huc29uIiwiZW1haWwiOiJhbGljZUBncmFubnkuanMiLCJpYXQiOjE1NjQ0NzU1MDF9.3VGPg-Zgb3XKaXUrcRTJYLjYFOQiB-axwahnvLXQMYs",
+            "Content-Type": "application/json"
+          }),
+          body: null,
         },
       ]);
     });
