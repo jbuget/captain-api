bc94bb01ef00c815e7d688aeb9aed60128773efa
jbuget@gmail.com <jbuget@gmail.com>
2019-10-16 02:27:10 +0200

    Add setup script

--- README.md
+++ README.md
@@ -13,6 +13,5 @@
 
 ```bash
 git clone git@github.com:jbuget/captain-api.git
-(cd db && ./reset.sh)
-(cd api && npm install)
+./setup.sh
 ```
---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Prerequisites
+
+echo 'Check prerequisites…'
+
+check_command () {
+    if ! [ -x "$(command -v $1)" ]; then
+      echo "Error: ${1} is not installed." >&2
+      exit 1
+    fi
+}
+
+commands=( docker git node npm vue )
+
+for i in "${commands[@]}" ; do check_command ${i} ; done
+
+echo 'Prerequisites checked.'
+
+
+# Database
+
+echo 'Setup database…'
+
+cd db
+./reset.sh
+cd ..
+
+echo 'Database configured'
+
+
+# API
+
+echo 'Setup API…'
+
+cd api
+npm install --no-optionnal
+cd ..
+
+echo 'API configured'
