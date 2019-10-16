2cd90457ea099845193e858cdd45631a6833d521
jbuget@gmail.com <jbuget@gmail.com>
2019-10-16 02:54:38 +0200

    Remove About page

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="login">
     <h1>Login page</h1>
+    <form id="form"
+          action="https://vuejs.org/"
+          method="post">
+      <p>
+        <label for="username">Username</label>
+        <input
+            id="username"
+            v-model="username"
+            type="username"
+            name="username"
+        >
+      </p>
+      <p>
+        <label for="password">Password</label>
+        <input
+            id="password"
+            v-model="password"
+            type="password"
+            name="password"
+        >
+      </p>
+      <p>
+        <input
+            type="submit"
+            value="Submit"
+        >
+      </p>
+    </form>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'Login',
+
+    data() {
+      return {
+        username: null,
+        password: null
+      };
+    }
+  }
+</script>
--- web/src/views/Signup.vue
+++ web/src/views/Signup.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="signup">
     <h1>Signup page</h1>
+    <form id="form"
+          action="https://vuejs.org/"
+          method="post">
+      <p>
+        <label for="name">Name</label>
+        <input
+            id="name"
+            v-model="name"
+            type="text"
+            name="name"
+        >
+      </p>
+      <p>
+        <label for="email">E-mail</label>
+        <input
+            id="email"
+            v-model="email"
+            type="email"
+            name="email"
+        >
+      </p>
+      <p>
+        <label for="password">Password</label>
+        <input
+            id="password"
+            v-model="password"
+            type="password"
+            name="password"
+        >
+      </p>
+      <p>
+        <input
+            type="submit"
+            value="Submit"
+        >
+      </p>
+    </form>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'Signup',
+
+    data() {
+      return {
+        name: null,
+        email: null,
+        password: null
+      };
+    }
+  }
+</script>
